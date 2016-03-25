8a9ef900938acf3c0c4490eaf8e7cfda003a9b30
Mog333 <robert.ag.post@gmail.com>
2016-03-25 00:19:34 -0600

    createTransferBaselines Function

--- scripts.py
+++ scripts.py
@@ -37,7 +37,6 @@ def findBestEpochsOfResultsFile(inputfile):
 
 
 
-
 def createGameFolders(projectDirectoryString, oldDir, newDir):
     folders = os.listdir(projectDirectoryString + oldDir)
 
@@ -49,47 +48,76 @@ def createGameFolders(projectDirectoryString, oldDir, newDir):
 
 
 
-def createPBSHeader(jobDirectory):
-    text = """
-#PBS -S /bin/bash
-#PBS -A ntg-662-aa
-#PBS -l nodes=1:gpus=1,mem=12gb
-#PBS -l walltime=03:00:00
-#PBS -M rpost@cs.ualberta.ca
-#PBS -m n
-#PBS -t 1\n""" + \
-        "#PBS -o " + jobDirectory + "output.txt\n" + \
-        "#PBS -e " + jobDirectory + "error.txt\n"
+
+def createTransferBaselineJobFiles(baseDirectory = 0, gameList = 0, modeList= 0, diffList = 0):
+    baseDirectory = "/gs/project/ntg-662-aa/RLRP/transferBaselines2"
+    gameList = ["boxing", "freeway", "hero", "space_invaders"]
+    flavorList = [  [[0, 1], [0, 2], [0,3]], \
+                    [[0, 1]], \
+                    [[0, 1],[0, 2],[0, 3],[0, 4]], \
+                    [[0,6], [0, 7]]
+                ]
+
+    seeds = [2]
+    epochs = 200
+    baseRomPath = "/home/rpost/roms/"
+    for gameIndex in len(gameList):
+        game = gameList[gameIndex]
+        gamesFlavors = flavorList[gameIndex]
+        for flavor in gamesFlavors:
+            mode = flavor[0]
+            diff = flavor[1]
+
+            for seed in seeds:
+
+                experimentDirectory = baseDirectory + "/" + str(game) + "/mode_" + str(mode) + "_diff_" + str(diff) + "/seed_" + str(seed) + "/"
+                jobFilename = str(game) + "m_" + str(mode) + "_d_" + str(diff) + "_s_" + str(seed) + ".pbs"
+                print experimentDirectory
+
+                if not os.path.isdir(experimentDirectory):
+                    os.makedirs(experimentDirectory)
+
+                header = createPBSHeader(experimentDirectory)
+                theanoFlag = createTheanoFlagString("gpu")
+                command = createJobCommandString(experimentDirectory, game, seed, epochs, baseRomPath, str(mode), str(diff)):
+
+                jobFile = open(experimentDirectory + jobFilename)
+                jobFile.write(header + "\n\n" + theanoFlag + " " + command)
+                jobFile.close()
+
+
+
+
+
+def createPBSHeader(jobDirectory, walltime = "5:00:00:00"):
+    text = "#PBS -S /bin/bash\n#PBS -A ntg-662-aa\n#PBS -l nodes=1:gpus=1\n#PBS -l walltime=" + walltime + "\n"
+    text+= "#PBS -M rpost@cs.ualberta.ca\n#PBS -m n\n#PBS -t 1\n"
+    text+= "#PBS -o " + jobDirectory + "jobOutput.txt\n"
+    text+= "#PBS -e " + jobDirectory + "error.txt\n"
 
     return text
 
-def createPythonPathExportString():
-    return """
-export PYTHONPATH=/home/rpost/DeepRL/agents/:$PYTHONPATH
-export PYTHONPATH=/home/rpost/DeepRL/utilities/:$PYTHONPATH
-export PYTHONPATH=/home/rpost/DeepRL/network/:$PYTHONPATH
-    """
 
-def createTheanoFlagString():
-    return "THEANO_FLAGS='device=gpu,floatX=float32,optimizer_including=cudnn,dnn.conv.algo_fwd=time_once,dnn.conv.algo_bwd=time_once' "
+def createTheanoFlagString(device="gpu"):
+    return "THEANO_FLAGS='device=%s,floatX=float32,optimizer_including=cudnn'" %(device)
 
 
-def createEvaluationJobCommandString(projectDirectoryString, romString, baseRomPath):
-    return "python /home/rpost/DeepRL/run_dqtn.py --network-type dnn " \
-    + "--rom %s --base-rom-path %s " %(romString, baseRomPath) \
-    + "--nn-file %s " %( projectDirectoryString + "evaluationNetwork.pkl")  \
-    + "--evaluationFrequency 1 --seed 1 --epochs 2 --deathEndsEpisode " \
-    + "--numHoldoutQValues %s " %("32") \
-    + "--steps-per-epoch 0 " \
-    + "> " + projectDirectoryString + "experimentOutput.txt"
+def createJobCommandString(projectDirectoryString, rom, seed, epochs, baseRomPath, modeStr="", diffStr = ""):
+    command = "python /home/rpost/RLRP/Base/runALEExperiment.py --networkType dnn " \
+    + "--rom %s --baseRomPath %s " %(rom, baseRomPath) \
+    + "--experimentDirectory %s " %(projectDirectoryString)  \
+    + "--evaluationFrequency 1 --seed %s --numEpochs %s "%(seed, epochs) \
+    + "--modeString %s --difficultyString %s " %(modeStr, diffStr)\
+    + "> " + projectDirectoryString + "/experimentOutput.txt"
+    return command
 
-def createEvaluationPBSFile(projectDirectoryString, romName, baseRomPath):
-    pbsContents = createPBSHeader(projectDirectoryString) + "\n" \
-                    + createPythonPathExportString() + "\n" \
-                    + createTheanoFlagString() \
-                    + createEvaluationJobCommandString(projectDirectoryString, romName, baseRomPath)
+# def createEvaluationPBSFile(projectDirectoryString, romName, baseRomPath):
+#     pbsContents = createPBSHeader(projectDirectoryString) + "\n" \
+#                     + createPythonPathExportString() + "\n" \
+#                     + createTheanoFlagString() \
+#                     + createEvaluationJobCommandString(projectDirectoryString, romName, baseRomPath)
 
-    return pbsContents
+#     return pbsContents
 
 
 
@@ -101,20 +129,20 @@ def createEvaluationPBSFile(projectDirectoryString, romName, baseRomPath):
 #copy best network file for that game into the new folder
 #create the pbs file to run the evaluation job
 
-def createAllGameEvaluationPBSFiles(projectDirectoryString, oldDir, newDir, baseRomPath):
-    games = os.listdir(projectDirectoryString + oldDir)
+# def createAllGameEvaluationPBSFiles(projectDirectoryString, oldDir, newDir, baseRomPath):
+#     games = os.listdir(projectDirectoryString + oldDir)
 
-    gameFolders = map(lambda item: projectDirectoryString + newDir + item + "/seed_1/", games)
+#     gameFolders = map(lambda item: projectDirectoryString + newDir + item + "/seed_1/", games)
 
-    for game,folder in zip(games,gameFolders):
+#     for game,folder in zip(games,gameFolders):
         
-        if not os.path.isdir(folder):
-            continue
+#         if not os.path.isdir(folder):
+#             continue
 
-        fileContents = createEvaluationPBSFile(folder, game, baseRomPath)
-        outputFile = open(folder + game + ".pbs", "w")
-        outputFile.write(fileContents)
-        outputFile.close()
+#         fileContents = createEvaluationPBSFile(folder, game, baseRomPath)
+#         outputFile = open(folder + game + ".pbs", "w")
+#         outputFile.write(fileContents)
+#         outputFile.close()
 
 
 
@@ -223,7 +251,7 @@ def getCompiledResultsFolder(projectDirectoryString, outputPath):
         newPath = outputPath + "/" + file[-3] + "/" + file[-2]
         if not os.path.isdir(newPath):
             os.makedirs(newPath)
-        fileToCopy = projectDirectoryString + "/" + "/".join(file[1:])#file[1] + "/" + file[2] + "/" + file[3]
+        fileToCopy = "/".join(file[1:])#file[1] + "/" + file[2] + "/" + file[3]
         newFilename = newPath + "/" + file[-1]
         print fileToCopy
         print newFilename
